64838ade7e02d6097349b87d594da82eaa391817
Vinicios Toledo <viabolafio@hotmail.com>
2023-01-29 00:33:41 -0300

    Criação do arquivo de contexto do Quiz

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { QuizProvider } from './context/quiz';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QuizProvider>
+      <App />
+    </QuizProvider>
   </React.StrictMode>
 );
 
